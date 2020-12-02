12c8a50d8ea015984940b15c5d3281af150d5ec3
Raphael Henrique <raphael.henrique@dtidigital.com.br>
2020-12-02 00:03:42 -0300

    Configuracoes iniciais, organizacao de pastas e configuracao do launchScreen

---
+++ app-register-cpf/app-register-cpf/System/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app-register-cpf
+//
+//  Created by Raphael Henrique on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
