5192b39de1dfc84b03989fb0245839bfac5ec17b
Raphael Henrique <raphael.henrique@dtidigital.com.br>
2020-12-02 00:29:01 -0300

    Adicionado fontes ao projeto

---
+++ app-register-cpf/app-register-cpf/Modules/RegisterCpf/RegisterCpfViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterCpfViewController.swift
+//  app-register-cpf
+//
+//  Created by Raphael Henrique on 12/2/20.
+//
+
+import UIKit
+
+class RegisterCpfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app-register-cpf/app-register-cpf/Modules/RegisterCpf/RegisterCpfViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterCpfViewModel.swift
+//  app-register-cpf
+//
+//  Created by Raphael Henrique on 12/2/20.
+//
+
+import Foundation
