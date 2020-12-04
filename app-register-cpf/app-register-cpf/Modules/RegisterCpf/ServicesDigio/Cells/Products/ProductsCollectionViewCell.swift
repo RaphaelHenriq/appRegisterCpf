829a5a47890e2f68bcd9187cbf01a58564141139
Raphael Henrique <raphael.henrique@dtidigital.com.br>
2020-12-04 14:49:33 -0300

    Construção células da tela de serviços Digio

--- app-register-cpf/app-register-cpf/Modules/RegisterCpf/ServicesDigio/Cells/Products/ProductsCollectionViewCell.swift
+++ app-register-cpf/app-register-cpf/Modules/RegisterCpf/ServicesDigio/Cells/Products/ProductsCollectionViewCell.swift
@@ -7,11 +7,48 @@
 
 import UIKit
 
-class ProductsCollectionViewCell: UICollectionViewCell {
+protocol ProductsCollectionViewCellDelegate: class {
+    func tapButtonProduct(name: String?)
+}
 
+class ProductsCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var productView: UIView!
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    // MARK: - Class properties
+    
+    weak var delegate: ProductsCollectionViewCellDelegate?
+    private var nameProduct: String?
+    
+    // MARK: - Init methods
+    
+    override var reuseIdentifier: String {
+        return ProductsCollectionViewCell.className
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.contentView.backgroundColor = .clear
+        self.productView.backgroundColor = .white
+        self.productView.allCorner(cornerRadius: 15)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapButton))
+        self.productView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func tapButton() {
+        self.delegate?.tapButtonProduct(name: self.nameProduct)
+    }
+    
+    // MARK: - Public properties
+    
+    func passData() {
+        
     }
 
 }
--- app-register-cpf/app-register-cpf/Modules/RegisterCpf/ServicesDigio/Cells/Spotlight/SpotlightCollectionViewCell.swift
+++ app-register-cpf/app-register-cpf/Modules/RegisterCpf/ServicesDigio/Cells/Spotlight/SpotlightCollectionViewCell.swift
@@ -6,12 +6,50 @@
 //
 
 import UIKit
+import Foundation
+
+protocol SpotlightCollectionViewCellDelegate: class {
+    func tapButtonSpotlight(name: String?)
+}
 
 class SpotlightCollectionViewCell: UICollectionViewCell {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var spotlightView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - Class properties
+    
+    weak var delegate: SpotlightCollectionViewCellDelegate?
+    private var nameSpotlight: String?
+    
+    // MARK: - Init methods
+    
+    override var reuseIdentifier: String {
+        return SpotlightCollectionViewCell.className
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.contentView.backgroundColor = .clear
+        self.spotlightView.backgroundColor = .white
+        self.spotlightView.allCorner(cornerRadius: 15)
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapButton))
+        self.spotlightView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func tapButton() {
+        self.delegate?.tapButtonSpotlight(name: self.nameSpotlight)
+    }
+    
+    // MARK: - Public methods
+    
+    func passData() {
+
     }
 
 }
